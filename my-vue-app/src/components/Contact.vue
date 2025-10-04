bebe5f60162274c352ed335ca4469e5596d61e43
Геннадий <crocodile-gena-92@mail.ru>
2025-10-04 09:52:34 +0000

    Initial commit

---
+++ my-vue-app/src/components/Contact.vue
@@ -0,0 +1,23 @@
+<template>
+  <li>
+    <span>{{ contact.name }}: {{ contact.phone }}</span>
+    <button @click="editContact">Редактировать</button>
+    <button @click="deleteContact">Удалить</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    contact: Object
+  },
+  methods: {
+    editContact() {
+      this.$emit('edit', this.contact);
+    },
+    deleteContact() {
+      this.$emit('delete', this.contact);
+    }
+  }
+}
+</script>
---
+++ my-vue-app/src/components/ContactForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <form @submit.prevent="submit">
+    <input v-model="contact.name" placeholder="Имя" required>
+    <input v-model="contact.phone" placeholder="Телефон" required>
+    <button type="submit">{{ isEditing ? 'Сохранить' : 'Добавить' }}</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    contact: Object,
+    isEditing: Boolean
+  },
+  methods: {
+    submit() {
+      this.$emit('submit', this.contact);
+    }
+  }
+}
+</script>
